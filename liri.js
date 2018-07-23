0a6119255fd05ef05501c488b18a006e751fbd2c
Charlie <cmainegra@protonmail.com>
2018-07-23 00:35:08 -0400

    starting to work

--- liri.js
+++ liri.js
@@ -1,4 +1,9 @@
+
 require("dotenv").config();
-var twitter = require('twitter');
+var keys = require('./keys.js');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
 
 console.log(process.argv);
