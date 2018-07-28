4125f076da4530e174a3d4ac86cf5bcb27cc8a85
Charlie <cmainegra@protonmail.com>
2018-07-28 07:53:52 -0400

    updates

--- liri.js
+++ liri.js
@@ -5,40 +5,50 @@ var request = require('request');
 var fs = require('fs');
 var Twitter = require('twitter')
 
-var spotify = new Spotify("./keys.Spotify");
+var spotify = new Spotify("./keys.spotify");
 var client = new Twitter("./keys.twitter");
 
-if(process.argv[2] === "my-tweets") {
- var params = {screen_name: 'everythingbage9'};
-  client.get('statuses/read', params, function(error, tweets, response) {
-    if(!error) {
-        console.log(tweets);
+if (process.argv[2] === "my-tweets") {
+    var params = { screen_name: 'everythingbage9' };
+    client.get('statuses/read', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+        // this should work under the documentation -- error code 34, says that page doesnt exist. unable to verify my keys are correct
     }
-    // this should work under the documentation -- error code 34, says that page doesnt exist. unable to verify my keys are correct
-  }
-)};
-if(process.argv[2] === 'spotify-this') {
-    for(let d = 2; d < process.argv.length; d++){
-    var songName = process.argv[d]
+    )
+};
+if (process.argv[2] === 'spotify-this') {
+    for (let d = 2; d < process.argv.length; d++) {
+        var songName = process.argv[d]
     }
-    spotify.search({ type: 'track', query: songName }, function(err, data) {
-        if ( err ) {
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
             console.log('Error occurred: ' + err);
             return;
-        } 
+        }
         else {
             console.log(JSON.stringify(data));
         }
-    })};
-    //having trouble with keys, and getting responses in general. i check back in later.
-if(process.argv[2] === 'movie-this'){
-    for(let c = 2; c < process.argv.length; c++){
-    var movieName = process.argv[c]; 
-}
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";  
-
-request(queryUrl, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(JSON.parse(body));
+    })
+};
+//having trouble with keys, and getting responses in general. i check back in later.
+if (process.argv[2] === 'movie-this') {
+    for (let c = 2; c < process.argv.length; c++) {
+        var movieName = "'" + process.argv[c]; + "'"
     }
-})};
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body).Title);
+            console.log(JSON.parse(body).Year);
+            console.log(JSON.parse(body).Rating);
+            console.log(JSON.parse(body).Country);
+            console.log(JSON.parse(body).Language);
+            console.log(JSON.parse(body).Plot);
+            console.log(JSON.parse(body).Actors);
+        }
+    })
+};
+
