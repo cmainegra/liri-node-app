bc811d39a7acb43e359982375b31246937902990
Charlie <cmainegra@protonmail.com>
2018-07-28 10:09:53 -0400

    workable

--- liri.js
+++ liri.js
@@ -5,14 +5,14 @@ var request = require('request');
 var fs = require('fs');
 var Twitter = require('twitter')
 
-var spotify = new Spotify("./keys.spotify");
-var client = new Twitter("./keys.twitter");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 if (process.argv[2] === "my-tweets") {
     var params = { screen_name: 'everythingbage9' };
     client.get('statuses/read', params, function (error, tweets, response) {
-        if (!error) {
-            console.log(tweets);
+        if (error) {
+            console.log(response.tweets);
         }
         // this should work under the documentation -- error code 34, says that page doesnt exist. unable to verify my keys are correct
     }
@@ -51,4 +51,3 @@ if (process.argv[2] === 'movie-this') {
         }
     })
 };
-
