0cf0e9c9699be852b94a5983d68b2c76b9970140
Charlie <cmainegra@protonmail.com>
2018-07-27 20:27:25 -0400

    somewhat complete

--- liri.js
+++ liri.js
@@ -1,9 +1,45 @@
-
 require("dotenv").config();
 var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+var Twitter = require('twitter')
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var spotify = new Spotify("./keys.Spotify");
+var client = new Twitter("./keys.twitter");
+
+if(process.argv[2] === "my-tweets") {
+ var params = {screen_name: 'everythingbage9'};
+  client.get('statuses/read', params, function(error, tweets, response) {
+    if(!error) {
+        console.log(tweets);
+    }
+    // this should work under the documentation -- error code 34, says that page doesnt exist. unable to verify my keys are correct
+  }
+)};
+if(process.argv[2] === 'spotify-this') {
+    for(let d = 2; d < process.argv.length; d++){
+    var songName = process.argv[d]
+    }
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        } 
+        else {
+            console.log(JSON.stringify(data));
+        }
+    })};
 
 
-console.log(process.argv);
+if(process.argv[2] === 'movie-this'){
+    for(let c = 2; c < process.argv.length; c++){
+    var movieName = process.argv[c]; 
+}
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";  
+
+request(queryUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(JSON.parse(body));
+    }
+})};
